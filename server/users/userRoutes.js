4ff7d89d7f5e47086af4f3ff7b4a6424b2b0d72e
Adam Smith <adamsmith910@aol.com>
2016-05-20 12:11:40 -0500

    refactored User routes and added params for User controller methods; Will have base functionality once all methods implemented

--- server/users/userRoutes.js
+++ server/users/userRoutes.js
@@ -5,54 +5,14 @@ var mongoose = require('mongoose');
 var Event = mongoose.model('events');
 var User = mongoose.model('users');
 
-router.post('/register', function(req, res, next) {
-	//createUser
-	if (!req.body.email || !req.body.password || !req.body.first_name ||
-		!req.body.last_name || !req.body.alias || !req.body.hometown) {
-		return res.status(400).json({})
-	}
+router.post('/register', createUser);
 
-	var user = new User;
-	user.save(function(err) {
-		if (err) {
-			console.log("Error saving user: " + err);
-			return next(err);
-		} else {
-			res.send("SUCCESS!");
-		}
-	});
-});
+router.post('/login', authenticateUser);
 
-router.post('/login', function(req, res) {
-	//checkAuth
-	if(!req.body.username || !req.body.password) {
-		return res.status(400).json({message: "Please fill out all fields"});
-	}
-});
+router.param('user', getUser);
 
+router.delete('/users/:user', deleteUser)
 
-router.param('user', function(req, res, next, id) {
-	var query = Post.findById(id);
-
-	query.exec(function(err, user) {
-		if (err) { return next(err); }
-		if (!user) { return next(new Error("can't find user")); }
-
-		req.user = user;
-		return next();
-	})
-});
-
-router.get('/users/:user', function(req, res, next) {
-	//getUser
-});
-
-router.delete('/users/:user', function(req, res, next) {
-	//deleteUser
-})
-
-router.post('/users/:user/events', function(req, res, next) {
-	//joinEvent
-})
+router.post('/users/:user/events', joinEvent)
 
 module.exports = router;
