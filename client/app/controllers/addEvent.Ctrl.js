53ce7398cb49c604c2794fb36046256556493f15
Sin Cheung Ko <scko823@gmail.com>
2016-05-20 14:56:29 -0500

    prelim ng-submit for the addEvent form

--- client/app/controllers/addEvent.Ctrl.js
+++ client/app/controllers/addEvent.Ctrl.js
@@ -1,4 +1,21 @@
 angular.module('break')
 .controller('addEvent', function($scope){
 	$scope.text ="Text";
+	$scope.eventCategoriesObj = {
+		Foods:false,
+		Outdoors:false,
+		Arts:false
+	};
+	$scope.addEvent = function () {
+		$scope.categories = Object.keys($scope.eventCategoriesObj).filter(function(key){
+			return $scope.eventCategoriesObj[key]
+		})
+		console.log('event_name', $scope.event_name);
+		console.log('location', $scope.location);
+		console.log('pic_url', $scope.pic_url);
+		console.log('description', $scope.description);
+		console.log('start_time', $scope.start_time);
+		console.log('end_time', $scope.end_time);
+		console.log('categories', $scope.categories);
+	}
 })
