6cc0d878028716c8151aaa2a106be9b4071bd750
Sin Cheung Ko <scko823@gmail.com>
2016-05-22 16:04:53 -0500

    signup page and controller completed

--- client/app/controllers/Signup.Ctrl.js
+++ client/app/controllers/Signup.Ctrl.js
@@ -1,16 +1,31 @@
 angular.module('break')
 .controller('signup', function($scope,$http){
+	$scope.interests ={
+		Food:false,
+		Outdoors:false,
+		Music:false
+	}
 
-	
 	$scope.signup = function(){
-		console.log($scope.username, $scope.password);
+		// var interests = [];
+		var interests = Object.keys($scope.interests).filter(function(interest){
+			return $scope.interests[interest]
+		});
+		var data = {
+			alias:$scope.alias,
+			password:$scope.password,
+			email:$scope.email,
+			first_name:$scope.first_name,
+			last_name:$scope.last_name,
+			hometown:$scope.hometown,
+			pic_url:$scope.pic_url,
+			interests:interests
+		};
+		// console.log(data);
 		// $http({
 		// 	method:"POST",
 		// 	url:"/endpoint",
-		// 	data:{
-		// 		username:$scope.username,
-		// 		password:$scope.password
-		// 	}
+		// 	data:data
 		// }).then(function(resp){
 		// 	//success login function
 		// 	//redirect to landing page
