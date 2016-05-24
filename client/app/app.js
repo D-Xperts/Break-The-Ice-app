a6a210934559b063b526150dd4f7b3df4ab2a17e
Sin Cheung Ko <scko823@gmail.com>
2016-05-24 10:34:34 -0500

    moved partial views to app/Views folder. started to config to SPA

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,9 @@
-angular.module('break',['angularMoment']);
+angular.module('break',['angularMoment', 'ngRoute'])
+.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'app/Views/landing.html',
+      controller: 'landing'
+    })
+
+})
