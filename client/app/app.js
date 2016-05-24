c08f633cfcfba7a1396ff52bfd14e94095a84906
Sin Cheung Ko <scko823@gmail.com>
2016-05-24 10:51:14 -0500

    wired up views to be SPA

--- client/app/app.js
+++ client/app/app.js
@@ -5,5 +5,24 @@ angular.module('break',['angularMoment', 'ngRoute'])
       templateUrl: 'app/Views/landing.html',
       controller: 'landing'
     })
-
+    .when('/signin', {
+      templateUrl: 'app/Views/signin.html',
+      controller: 'signin'
+    })
+    .when('/signup', {
+      templateUrl: 'app/Views/signup.html',
+      controller: 'signup'
+    })
+    .when('/event', {
+      templateUrl: 'app/Views/EventView.html',
+      controller: 'EventCtrl'
+    })
+    .when('/addEvent', {
+      templateUrl: 'app/Views/addEvent.html',
+      controller: 'addEvent'
+    })
+    .when('/user', {
+      templateUrl: 'app/Views/UserView.html',
+      controller: 'UserCtrl'
+    })
 })
