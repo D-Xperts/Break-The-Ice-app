8d0fe6259530c9bcff3cd06275f4159976386fc9
Adam Smith <adamsmith910@aol.com>
2016-05-25 14:32:56 -0500

    fixed error in event routes

--- server/events/eventRoutes.js
+++ server/events/eventRoutes.js
@@ -1,8 +1,11 @@
+var express = require('express');
+var router = express.Router();
 var eventController = require('./eventController.js')
 
-module.exports = function(app) {
+router.get('/api/v1/events/', eventController.getEvents);
 
-  app.get('/api/v1/events/', eventController.getEvents);
-  app.post('/api/v1/events/', eventController.createEvent);
-  app.delete('api/v1/events/', eventController.deleteEvent);
-}
+router.post('/api/v1/events/', eventController.createEvent);
+
+router.delete('api/v1/events/', eventController.deleteEvent);
+
+module.exports = router;
