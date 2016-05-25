ad9d97426f7f7242aae1bbb0b54ff1ff59ce551e
Adam Smith <adamsmith910@aol.com>
2016-05-24 23:58:20 -0500

    added stuff so server can run

--- server/server.js
+++ server/server.js
@@ -1,9 +1,8 @@
   var express     = require('express'),
       mongoose    = require('mongoose');
 
+
 var app = express();
-
-
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
