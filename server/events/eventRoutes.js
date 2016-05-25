d30a370403bbb18c80e20348faa7d38c84b485a7
Sin Cheung Ko <scko823@gmail.com>
2016-05-25 16:06:24 -0500

    modified Events Model and Controller. Able to POST /api/v1/events to create an event; GET /api/v1/events to get a list of events

--- server/events/eventRoutes.js
+++ server/events/eventRoutes.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 var eventController = require('./eventController.js')
 
-router.get('/api/v1/events/', eventController.getEvents);
+router.get('/', eventController.getEvents);
 
-router.post('/api/v1/events/', eventController.createEvent);
+router.post('/', eventController.createEvent);
 
-router.delete('api/v1/events/', eventController.deleteEvent);
+router.delete('/', eventController.deleteEvent);
 
 module.exports = router;
