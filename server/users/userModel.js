1cfd75138355091f2461a0e0907e673937bcbb62
Sin Cheung Ko <scko823@gmail.com>
2016-05-25 16:10:02 -0500

    modified User Model and Controller. Added additional info to create new user, removed plaintext password field from users collections. Able to create new user by POST /api/v1/users

--- server/users/userModel.js
+++ server/users/userModel.js
@@ -7,10 +7,10 @@ var UserSchema = new mongoose.Schema({
 		type: String,
 		required: true
 	},
-	password: {
-		type: String,
-		required: true
-	},
+	// password: {
+	// 	type: String,
+	// 	required: true
+	// },
 	first_name: {
 		type: String,
 		required: true
