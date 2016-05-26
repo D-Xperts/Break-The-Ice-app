15b17062f660866792fe3a6e171726fcb57434ea
Sin Cheung Ko <scko823@gmail.com>
2016-05-26 15:36:24 -0500

    set the $rootScope token and req.header.authorization field as user log in

--- client/app/controllers/Signin.Ctrl.js
+++ client/app/controllers/Signin.Ctrl.js
@@ -1,5 +1,5 @@
 angular.module('break')
-.controller('signinCtrl', function($scope,$window, $location,UserAuth){
+.controller('signinCtrl', function($scope, $window, $location, $rootScope, UserAuth){
 	$scope.signin = function(){
 		// console.log($scope.email, $scope.password);
 		UserAuth.signin({
@@ -8,6 +8,9 @@ angular.module('break')
 		})
 		.then(function(token){
 			$window.localStorage.setItem('Break.The.Ice', token);
+			//need to set the token and set the header field
+			$rootScope.token = token;
+			$http.defaults.headers.common.Authorization = token;
 			$location.path('/')
 		})
 		.catch(function(err){
