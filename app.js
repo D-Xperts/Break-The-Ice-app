a19ef674d501cac0885c303e1f4ec80b252be040
Sin Cheung Ko <scko823@gmail.com>
2016-05-30 12:07:18 -0500

    config to use favicon

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ var users = require('./server/users/userRoutes');
 var app = express();
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'client', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
